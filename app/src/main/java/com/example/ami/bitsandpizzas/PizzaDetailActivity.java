2967288b168821475760d7a8af7b2c4e240dff88
Andrei_Mitrafanau <Andrei_Mitrafanau@mail.ru>
2016-11-28 13:51:26 +0300

    Add pizza detail activity.

---
+++ app/src/main/java/com/example/ami/bitsandpizzas/PizzaDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.ami.bitsandpizzas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PizzaDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_detail);
+    }
+}
