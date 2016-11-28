89bdb1aa6109f08ffac8ef59ccf7bf6cc6f4dee2
Andrei_Mitrafanau <Andrei_Mitrafanau@mail.ru>
2016-11-28 14:35:31 +0300

    Add pizza detail activity.

--- app/src/main/java/com/example/ami/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/ami/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,9 +1,11 @@
 package com.example.ami.bitsandpizzas;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,10 +14,20 @@ public class CaptionedImagesAdapter
         extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
+
+    public static interface Listener {
+        public void onClick(int posititon);
+    }
+
 
     public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
         this.captions = captions;
         this.imageIds = imageIds;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -36,7 +48,7 @@ public class CaptionedImagesAdapter
     }
 
     @Override
-    public void onBindViewHolder(CaptionedImagesAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(CaptionedImagesAdapter.ViewHolder holder, final int position) {
 
         CardView cardView = holder.cardView;
         ImageView imageView = (ImageView) cardView.findViewById(R.id.info_image);
@@ -45,6 +57,14 @@ public class CaptionedImagesAdapter
         imageView.setContentDescription(captions[position]);
         TextView textView = (TextView) cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
     }
 
     @Override
